dccf41dd4b54f33f151bd205a7794647929c0e34
tj mahr <mahr@wisc.edu>
2014-02-17 14:06:04 -0600

    pixels per mm constant

--- R/constants.r
+++ R/constants.r
@@ -5,6 +5,7 @@ lwl_constants <-
   list(ms_per_frame = 16.6546,
        screen_width = 1920,
        screen_height = 1200,
+       pixels_per_mm = 3.71,
        l2t_subject_dir = "^[0-9]{3}[CLPXclpx]",
        l2t_subject = "[0-9]{3}[CLPDE][0-9]{2}[MFX][SA][1-9]"
   )
