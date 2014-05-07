d98d91a00767e5026de5c5592417b6cd70a7d761
tj mahr <tjmahr@users.noreply.github.com>
2014-05-07 13:06:33 -0500

    Update utilities.R

--- R/utilities.R
+++ R/utilities.R
@@ -3,3 +3,19 @@
 `%lacks%` <- function(x, y) !any(y %in% x)
 
 is_all_na <- function(x) all(is.na(x))
+
+#' Divide a vector into equal-sized chunks, except for maybe the last one
+#' 
+#' @param xs a vector
+#' @param step the length of a chunk
+#' @return a list with the elements of `xs` partitioned into `step`-sized chunks
+slice <- function(xs, step = 5) {
+  last_tail <- length(xs)
+  heads <- seq(1, last_tail, step)
+  expand_head <- function(this_head) { 
+    next_tail <- this_head + (step - 1)
+    this_tail <- min(next_tail, last_tail) 
+    xs[seq(this_head, this_tail)]
+  }
+  lapply(heads, expand_head)
+}
