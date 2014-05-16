5e98b4e727c03ee3095e0ffe613c7ee47075f8c0
tj mahr <tjmahr@users.noreply.github.com>
2014-05-16 10:54:11 -0500

    orthogonal_time(...)

--- R/gca.R
+++ R/gca.R
@@ -8,3 +8,18 @@ empirical_logit_weight <- function(x, y) {
   var2 <- 1 / (y + 0.5)
   var1 + var2
 }
+
+orthogonal_time <- function(times, degree) {
+  clean_times <- sort(unique(times))
+  time_df <- as.data.frame(poly(clean_times, degree))
+  names(time_df) <- paste0("ot", names(time_df))
+  time_df$Time <- clean_times
+  time_df
+}
+
+frame_time <- function(time, bins = 3) {
+  (time / lwl_constants$ms_per_frame) / bins
+}
+
+# inverse_logit <- gtools::inv.logit
+
