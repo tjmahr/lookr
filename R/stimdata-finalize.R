e8569294942adb4a7ec851c33be4cb576d56c5e2
unknown <mahr@wisc.edu>
2014-06-06 17:02:48 -0500

    store DateTime attribute as character instead of a date

--- R/stimdata-finalize.R
+++ R/stimdata-finalize.R
@@ -31,21 +31,21 @@ FinalizeStimdata.MP <- function(stimdata) {
   # flip the target-image location label for mispronounced and nonsense word
   # trials. In cheatsheet form:
   # ```
-  #               Before correction:            After correction: 
-  #   StimType    Target        Distractor      Target        Distractor 
-  #   'real'      familiar      unfamiliar      familiar      unfamiliar  
+  #               Before correction:            After correction:
+  #   StimType    Target        Distractor      Target        Distractor
+  #   'real'      familiar      unfamiliar      familiar      unfamiliar
   #   'MP'        unfamiliar    familiar        familiar      unfamiliar
-  #   'nonsense'  unfamiliar    familiar        familiar      unfamiliar  
+  #   'nonsense'  unfamiliar    familiar        familiar      unfamiliar
   # ```
   targets <- stimdata$Target
   flip_me <- stimdata$StimType %in% c("MP", "nonsense")
-  targets <- ifelse(!flip_me, targets, 
+  targets <- ifelse(!flip_me, targets,
                     ifelse(targets == "ImageL", "ImageR", "ImageL"))
   stimdata$TargetImage <- targets
-  
+
   # Create a field for the Distractor Image
   stimdata$DistractorImage <- ifelse(targets == "ImageL", "ImageR", "ImageL")
-  
+
   # Use the image filenames and the target image location to determine the
   # targeted word.
   .GetStimImg <- function(trial_num, col_name) {
@@ -53,27 +53,27 @@ FinalizeStimdata.MP <- function(stimdata) {
     # Drop of the number at the end of the filename
     str_sub(stim, 1, -2)
   }
-  
-  stimdata <- transform(stimdata, Target = mapply(.GetStimImg, TrialNo, Target))  
-  
+
+  stimdata <- transform(stimdata, Target = mapply(.GetStimImg, TrialNo, Target))
+
   # Filenames for audio files separate stimuli information with underscores:
   # `SAE_Fin_cat_112_20`. We reduce the filename down to the target word by
   # erasing numbers, dialect abbreviations, carrier phrase abbreviations and
   # underscores. We choose this strategy (erasure rather than extraction) in
   # case we cannot rely on the order of stimuli information.
-  stimdata$TargetWord <- str_replace_all(stimdata$Audio, 
+  stimdata$TargetWord <- str_replace_all(stimdata$Audio,
                                          "[0-9]+|SAE|AAE|Fin|See|_+", "")
-  
+
   # Reorder the stimdata
   first <- c("Task", "Protocol", "DateTime", "Subject", "Block", "TrialNo")
   target_stim <- c("TargetWord")
   place_stim <- c("ImageL", "ImageR")
   img_stim <- c("Target", "DistractorImage")
   names(stimdata)
-  
+
   ordering <- c(first, target_stim, place_stim, img_stim)
   stimdata <- ReorderStimdata(stimdata, ordering)
-  
+
   stimdata
 }
 
@@ -84,84 +84,84 @@ FinalizeStimdata.MP <- function(stimdata) {
 
 FinalizeStimdata.RWL <- function(stimdata) {
   # Set some useful constants
-  FOUR_PLACES <- c("UpperLeftImage", "UpperRightImage", 
-                   "LowerRightImage", "LowerLeftImage") 
+  FOUR_PLACES <- c("UpperLeftImage", "UpperRightImage",
+                   "LowerRightImage", "LowerLeftImage")
   FOUR_TYPES <- c("Target", "SemanticFoil", "PhonologicalFoil", "Unrelated")
-  
+
   # Work with just the columns about images
   image_vars <- c("TrialNo", FOUR_PLACES, FOUR_TYPES)
   sub_stim <- stimdata[image_vars]
-  
+
   # At this point, all we know is the trial number, the filename for each image
   # location, and the location number for each stimulus type. For example:
   # ```
   #   TrialNo         : int  1 2 3 4 5 6 7 8 9 10 ...
-  #   
+  #
   #   UpperLeftImage  : chr  "pan2" "bell2" "bee2" "box2" ...
   #   UpperRightImage : chr  "bell2" "kite2" "fly2" "drum2" ...
   #   LowerRightImage : chr  "drum2" "flag2" "clown2" "pan2" ...
   #   LowerLeftImage  : chr  "bee2" "comb2" "belt2" "van2" ...
-  #   
+  #
   #   SemanticFoil    : chr  "Image3" "Image3" "Image2" "Image1" ...
   #   PhonologicalFoil: chr  "Image4" "Image4" "Image4" "Image3" ...
   #   Unrelated       : chr  "Image1" "Image1" "Image3" "Image2" ...
   #   Target          : chr  "Image2" "Image2" "Image1" "Image4" ...
   # ```
-  
+
   # To reduce the data, we (1) convert location number into location names, (2)
   # record what stimulus type occurs in each location for each trial, (3) map
   # the location names in the stimulus columns onto the image words (not the
   # filename). Output will look more like this:
   # ```
   #   TrialNo             : int  1 2 3 4 5 6 7 8 9 10 ...
-  #   
+  #
   #   UpperLeftImage      : chr  "pan2" "bell2" "bee2" "box2" ...
   #   UpperRightImage     : chr  "bell2" "kite2" "fly2" "drum2" ...
   #   LowerRightImage     : chr  "drum2" "flag2" "clown2" "pan2" ...
   #   LowerLeftImage      : chr  "bee2" "comb2" "belt2" "van2" ...
-  #   
+  #
   #   SemanticFoil        : chr  "drum" "flag" "fly" "box" ...
   #   PhonologicalFoil    : chr  "bee" "comb" "belt" "pan" ...
   #   Unrelated           : chr  "pan" "bell" "clown" "drum" ...
   #   Target              : chr  "bell" "kite" "bee" "van" ...
-  #   
+  #
   #   UpperLeftImagePlace : chr  "Unrelated" "Unrelated" "Target" ...
   #   UpperRightImagePlace: chr  "Target" "Target" "SemanticFoil" ...
   #   LowerRightImagePlace: chr  "SemanticFoil" "SemanticFoil" "Unrelated" ...
   #   LowerLeftImagePlace : chr  "PhonologicalFoil" "PhonologicalFoil" ...
   # ```
-  
-  # (1) Convert location numbers into location names. 
+
+  # (1) Convert location numbers into location names.
   .GetImagePlace <- function(image) {
-    switch(image, 
+    switch(image,
            'Image1' = 'UpperLeftImage',
            'Image2' = 'UpperRightImage',
            'Image3' = 'LowerRightImage',
-           'Image4' = 'LowerLeftImage')  
+           'Image4' = 'LowerLeftImage')
   }
   # Vectorized version of the above function
   .GetImagePlaces <- function(col) sapply(col, .GetImagePlace)
   sub_stim <- within(sub_stim, {
     # TargetImage preserves the location of the Target Image
-    TargetImage           <- .GetImagePlaces(Target) 
+    TargetImage           <- .GetImagePlaces(Target)
     SemanticFoilImage     <- .GetImagePlaces(SemanticFoil)
     PhonologicalFoilImage <- .GetImagePlaces(PhonologicalFoil)
     UnrelatedImage        <- .GetImagePlaces(Unrelated)
-    Target           <- .GetImagePlaces(Target) 
+    Target           <- .GetImagePlaces(Target)
     Unrelated        <- .GetImagePlaces(Unrelated)
     SemanticFoil     <- .GetImagePlaces(SemanticFoil)
     PhonologicalFoil <- .GetImagePlaces(PhonologicalFoil)
   })
-  
+
   # (2) Record the type of stimulus that falls in each image location.
   .GetStimForPlace <- function(place, df = sub_stim) {
     # Initialize a vector to hold stimulus names
-    num_stim <- nrow(df[place])  
+    num_stim <- nrow(df[place])
     place_stim <- character(num_stim)
-    
+
     # For each stimulus type, extract the trials in which that stimulus type
     # occurs in `place`
-    for(img in FOUR_TYPES) {
+    for (img in FOUR_TYPES) {
       rows <- which(df[img] == place)
       place_stim[rows] <- img
     }
@@ -169,32 +169,32 @@ FinalizeStimdata.RWL <- function(stimdata) {
   }
   place_cols <- paste0(FOUR_PLACES, "Stimulus")
   sub_stim[place_cols] <- Map(.GetStimForPlace, FOUR_PLACES)
-  
+
   # (3) Assign image names onto stimulus types.
   .GetStimImg <- function(i, j) {
     stim <- sub_stim[i, j]
     str_sub(stim, 1, -2)
   }
-  
+
   sub_stim <- within(sub_stim, {
     Target           <- mapply(.GetStimImg, TrialNo, Target)
     SemanticFoil     <- mapply(.GetStimImg, TrialNo, SemanticFoil)
     PhonologicalFoil <- mapply(.GetStimImg, TrialNo, PhonologicalFoil)
     Unrelated        <- mapply(.GetStimImg, TrialNo, Unrelated)
   })
-  
+
   # Merge these results back into the stimdata
-  stimdata[names(sub_stim)] <- sub_stim  
-  
+  stimdata[names(sub_stim)] <- sub_stim
+
   # Specify the most important stimuli and combine them into the ordering
   first <- c("Task", "Protocol", "DateTime", "Subject", "Block", "TrialNo")
-  img_stim <- c("UpperLeftImageStimulus", "UpperRightImageStimulus", 
+  img_stim <- c("UpperLeftImageStimulus", "UpperRightImageStimulus",
                 "LowerRightImageStimulus", "LowerLeftImageStimulus")
-  
+
   ordering <- c(first, FOUR_PLACES, FOUR_TYPES, img_stim)
-  
+
   stimdata <- ReorderStimdata(stimdata, ordering)
-  
+
   stimdata
 }
 
@@ -204,16 +204,16 @@ FinalizeStimdata.RWL <- function(stimdata) {
 FinalizeStimdata.Coartic <- function(stimdata) {
   # Create a field for the Distractor Image
   stimdata$DistractorImage <- ifelse(stimdata$TargetImage == "ImageL", "ImageR", "ImageL")
-  
+
   # Reorder the stimdata
   first <- c("Task", "DateTime", "Subject", "Block", "TrialNo")
   target_stim <- c("TargetWord")
   place_stim <- c("ImageL", "ImageR")
   img_stim <- c("TargetImage", "DistractorImage")
-  
+
   ordering <- c(first, target_stim, place_stim, img_stim)
   stimdata <- ReorderStimdata(stimdata, ordering)
-  
+
   stimdata
 }
 
--- R/stimdata.R
+++ R/stimdata.R
@@ -3,52 +3,52 @@
 
 
 #' Parse an LWL stimulus data file outputted by Eprime
-#' 
+#'
 #' \code{Stimdata} is used to read and parse the \code{.txt} file that is output
 #' by E-prime during a session of a Looking While Listening experiment.
-#' 
+#'
 #' @param stimdata_path Either the full or relative path to the \code{.txt} file
 #'   that is to be parsed.
-#' @param output_file Either \code{NULL} or the path---full or relative---for 
-#'   the output file. If \code{NULL}, then no output file is created. If a path 
-#'   is specified, then the parsed stimdata is written out as a \code{.csv} 
+#' @param output_file Either \code{NULL} or the path---full or relative---for
+#'   the output file. If \code{NULL}, then no output file is created. If a path
+#'   is specified, then the parsed stimdata is written out as a \code{.csv}
 #'   file. Default is \code{NULL}.
-#' @return A dataframe containing the parsed stimdata.  Each row of the 
+#' @return A dataframe containing the parsed stimdata.  Each row of the
 #'   dataframe is the stimdata for a single experimental trial.
-#'   
-#' @details 
-#' A stimdata file in a Looking While Listening task file the naming 
-#' convention: \code{[Task]_[BlockNo]_[SubjectID]}. The stimdata file is 
-#' assigned a class based on the task in the filename, and then methods for 
+#'
+#' @details
+#' A stimdata file in a Looking While Listening task file the naming
+#' convention: \code{[Task]_[BlockNo]_[SubjectID]}. The stimdata file is
+#' assigned a class based on the task in the filename, and then methods for
 #' extracting the stimdata are dispatched based on that class value. Valid task
 #' names include "RWL", "MP", "Coartic" and "VisWorld".
-#' 
+#'
 #' @importFrom tools file_path_sans_ext
 #' @export
-Stimdata <- function(stimdata_path, outputFile = NULL) {  
+Stimdata <- function(stimdata_path, outputFile = NULL) {
   # Extract experiment information from the input file name
   file_info <- ParseFilename(stimdata_path)
   task <- file_info$Task
-  
+
   # Load the stimdata file
   stimlog <- LoadStimdataFile(stimdata_path)
   class(stimlog) <- c(task, class(stimlog))
-  
+
   # Determine the appropriate stimdata features and extract them.
   config <- DetermineStim(stimlog)
   stimdata <- ExtractStim(config, stimlog)
-  
+
   # Finalize stimdata
   class(stimdata) <- c(task, "Stimdata", class(stimdata))
   stimdata <- FinalizeStimdata(stimdata)
   stimdata$Basename <- file_info$Basename
-  
+
   # Optionally write out stimdata as a tab-delimited table.
   if (!is.null(outputFile)) {
     write.table(stimdata, file = outputFile, sep = ',',
                 quote = FALSE, row.names = FALSE)
   }
-  
+
   stimdata
 }
 
@@ -56,13 +56,13 @@ Stimdata <- function(stimdata_path, outputFile = NULL) {
 
 
 #' Open a stimulus log file outputted by Eprime
-#' 
+#'
 #' @param stimdata_path Either the full or relative path to the \code{.txt} file
 #'   that is to be parsed.
-#' @return the raw contents of the stimdata file. The basename of 
+#' @return the raw contents of the stimdata file. The basename of
 #'   \code{stimdata_path} is attached as an attribute called \code{"Basename"}.
-#'   
-#' @details 
+#'
+#' @details
 #' Historically, we have had issues with the encoding of these .txt
 #' files, so now we include some exception-handling measures. The procedure is
 #' to first try to load the file with UCS-2 Little Endian encoding. If a warning
@@ -73,31 +73,31 @@ Stimdata <- function(stimdata_path, outputFile = NULL) {
 LoadStimdataFile <- function(stimdata_path) {
   # A message with the filename is helpful.
   message(paste0("Reading stimdata in ", stimdata_path))
-  
+
   # Initialize an empty warning object.
   warned <- NULL
-  
+
   # Define the procedure to handle warnings: Store them and muffle them.
-  HandleWarning <- function(w) { 
+  HandleWarning <- function(w) {
     warned <<- w
-    invokeRestart("muffleWarning")    
+    invokeRestart("muffleWarning")
   }
-  
-  # Read in a file connection using the warning handler. 
+
+  # Read in a file connection using the warning handler.
   con <- file(stimdata_path, open = 'rt', encoding = 'UCS-2LE')
   withCallingHandlers(stimlog <- readLines(con), warning = HandleWarning)
   close(con)
-  
+
   # If a warning is caught, try connection again with no encoding specified.
   if (0 < length(warned)) {
     con <- file(stimdata_path, open = 'rt')
     stimlog <- readLines(con)
     close(con)
   }
-  
+
   # Attach the original filename to the stimlog as an attribute.
   attr(stimlog, "Basename") <- basename(stimdata_path)
-  
+
   stimlog
 }
 
@@ -110,62 +110,56 @@ ExtractStim <- function(stim_config, stimlog) {
   # Get the values from the stimdata file
   stim <- stim_config$Stim
   parsed_stimlog <- Map(.GetValuesOfStimdataType(stimlog), stim)
-  
+
   # If a stim field was not found in the stimdata file, it will have a length of
-  # zero in the parsed stimlog. We drop those fields that weren't found. This 
-  # avoids the error that comes from trying to combine the stim vectors of 
+  # zero in the parsed stimlog. We drop those fields that weren't found. This
+  # avoids the error that comes from trying to combine the stim vectors of
   # differing lengths into a dataframe (which is a collection of vectors of
   # equal length).
-  lengths <- sapply(parsed_stimlog, length) 
+  lengths <- sapply(parsed_stimlog, length)
   drops <- which(lengths == 0)
-  
-  if (length(drops) != 0) { 
+
+  if (length(drops) != 0) {
     parsed_stimlog <- parsed_stimlog[-drops]
     # Warn the user
     warn_names <- paste(names(parsed_stimlog)[drops], collapse=", ")
     warn <- paste0("Empty stimdata fields: ", warn_names)
     warning(warn)
   }
-  
+
   # Put the dataframe together
   stimdata <- data.frame(parsed_stimlog, stringsAsFactors = FALSE)
-  
+
   # Add the constants
-  for(constant in names(stim_config$Constants)) {
+  for (constant in names(stim_config$Constants)) {
     # Using a for-loop because each iteration updates (side-effects) `stimdata`
-    value <- stim_config$Constants[constant]  
+    value <- stim_config$Constants[constant]
     stimdata[, constant] <- value
   }
-  
+
   # Convert number strings to numerics
   num_stim <- stim_config$Numerics
   stimdata[num_stim] <- lapply(stimdata[num_stim], as.numeric)
-  
+
   # Compute derived column values
   skip <- is.null(stim_config$Derived)
-  if(!skip) {
+  if (!skip) {
     exp <- parse(text = stim_config$Derived)
+    stimdata <- within(stimdata, {
+      for (ex in exp) eval(ex)
+      rm(ex)})
   }
-  
-  stimdata <- within(stimdata, {
-    # Do nothing if there are no values to derive
-    if(!skip) {
-      for(ex in exp) eval(ex)
-      rm(ex)  
-    }
-  })
-  
+
   # Add trial numbers
-  stimdata$TrialNo <- 1:nrow(stimdata)
-  
+  stimdata$TrialNo <- seq_len(nrow(stimdata))
+
   # Include the date and time of the block. `unique` because the date and time
   # are recorded twice (at the beginning and end of the experiment).
   date <- unique(.GetValuesOfStimdataType(stimlog)("SessionDate"))
-  date <- mdy(date, quiet = TRUE)
   time <- unique(.GetValuesOfStimdataType(stimlog)("SessionTime"))
-  time <- hms(time)
-  stimdata$DateTime <- date + time  
-  
+  datetime <- mdy(date, quiet = TRUE) + hms(time)
+  stimdata$DateTime <- as.character(datetime)
+
   stimdata
 }
 
@@ -173,48 +167,48 @@ ExtractStim <- function(stim_config, stimlog) {
 
 
 #' Extract values of a given type from a stimdata file
-#' 
-#' \code{.GetValuesOfStimdataType} is a utility function for extracting from a 
-#' stimlog, the value of a given type of stimdata for each trial. This function 
+#'
+#' \code{.GetValuesOfStimdataType} is a utility function for extracting from a
+#' stimlog, the value of a given type of stimdata for each trial. This function
 #' is curried so that \code{.GetValuesOfStimdataType(stimlog)} returns a
 #' function that can be applied to a vector of the names of the stimdata types.
-#' 
+#'
 #' @keywords internal
-#' @param stimlog A character vector whose elements are the lines of the 
+#' @param stimlog A character vector whose elements are the lines of the
 #'   stimlog.
 #' @param stimdataType A character string identifying the type of stimdata whose
-#'   value for each trial should be found.  E.g., 'Image1' is the stimdata type 
+#'   value for each trial should be found.  E.g., 'Image1' is the stimdata type
 #'   of the top left image in the Real Word Listening task.
 #' @return Either a character vector or numeric vector, each element of which is
 #'   the value of stimdataType for a trial.
 #' @examples
 #' # Single value extraction
 #' # onset_times <- .GetValuesOfStimdataType(stimlog)("Image2sec.OnsetTime")
-#' 
+#'
 #' # Multiple value extraction
 #' # image_names <- c("ImageL", "ImageR")
 #' # image_values <- Map(.GetValuesOfStimdataType(stimlog), image_names)
-#' 
-.GetValuesOfStimdataType <- function(stimlog) {  
+#'
+.GetValuesOfStimdataType <- function(stimlog) {
   # Define the function that is returned once a stimlog argument is passed to
   # `.GetValuesOfStimdataType.`
   .LambdaStimdataType <- function(stimdata_type) {
     # The stimdata for each trial is logged using the following pattern:
-    # `\t\t{stimdataType}: {value}`. Create a regular expression that can 
+    # `\t\t{stimdataType}: {value}`. Create a regular expression that can
     # be used to find the line in each trial where `stimdataType` is logged.
     stim_pattern <- sprintf('\t*%s: ', stimdata_type)
-    
+
     # Extract the lines of the stimlog where `stimdataType` is logged.
     stimlog_lines <- grep(stim_pattern, stimlog, value = TRUE)
     values <- sub(stim_pattern, '', stimlog_lines)
     return(values)
-  } 
+  }
   return(.LambdaStimdataType)
 }
 
 #' @keywords internal
 .CheckForStimdataType <- function(stimlog, stimdataType) {
-  # Wrapper for `.GetValuesOfStimdataType` that returns whether a value is 
+  # Wrapper for `.GetValuesOfStimdataType` that returns whether a value is
   # present in `stimlog`
   values <- .GetValuesOfStimdataType(stimlog)(stimdataType)
   found <- length(values) != 0
