41b6afe0e2255bb37a9362834c69ae96272a7485
unknown <mahr@wisc.edu>
2014-06-08 16:39:57 -0500

    export empirical logit and orthogonal time functions

--- R/gca.R
+++ R/gca.R
@@ -1,14 +1,33 @@
 
+#' Compute empirical logit
+#'
+#' @param x vector containing number of looks to target
+#' @param y vector containing number of looks to distractors
+#' @return \code{empirical_logit} returns the empirical logit of looking to
+#'   target. \code{empirical_logit_weight} returns weights for these values.
+#' @export
+#' @references Dale Barr's
+#' \href{http://talklab.psy.gla.ac.uk/tvw/elogit-wt.html}{Walkthrough of an
+#' "empirical logit" analysis in R}
 empirical_logit <- function(x, y) {
   log((x + 0.5) / (y + 0.5))
 }
 
+#' @rdname empirical_logit
+#' @export
 empirical_logit_weight <- function(x, y) {
   var1 <- 1 / (x + 0.5)
   var2 <- 1 / (y + 0.5)
   var1 + var2
 }
 
+#' Compute orthogonal times
+#'
+#' @param times a vector of time values
+#' @param degree of the desired polynomial
+#' @return a data-frame with original time values and an \code{ot} column for
+#'   each polynomial degree
+#' @export
 orthogonal_time <- function(times, degree) {
   clean_times <- sort(unique(times))
   time_df <- as.data.frame(poly(clean_times, degree))
