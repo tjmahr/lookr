68a6ee5cf8bc677674ae9761986ab80cd3f53d37
TJ Mahr <mahr@wisc.edu>
2014-07-01 17:45:28 -0500

    simplified InterpolateMissingFrames(...)

--- R/missing.R
+++ R/missing.R
@@ -1,35 +1,3 @@
-
-#' Calculate the amount of missing data in a list of Trials
-#'
-#' @param trial a Trial object with a \code{GazeByImageAOI} column
-#' @param trials a list of Trial objects, each with a \code{GazeByImageAOI}
-#'   column
-#' @param column A character string that names one of the columns of Gazedata in
-#'   the trial object. Default is `"GazeByImageAOI"`.
-#' @return A list of Trial objects that has been updated to include attributes
-#'   containing percentage of mistracked data.
-#' @export
-CalculateMistrackings <- function(...) UseMethod("CalculateMistrackings")
-
-#' @export
-CalculateMistrackings.list <- function(trials, column = "GazeByImageAOI") {
-  classes <- class(trials)
-  trials <- lapply(trials, CalculateMistrackings, column)
-  class(trials) <- classes
-  trials
-}
-
-#' @export
-CalculateMistrackings.Trial <- function(trial, column = "GazeByImageAOI") {
-  mistrackings <- sum(is.na(trial[[column]]))
-  num_frames <- length(trial[[column]])
-  trial %@% "MistrackedFrames" <- mistrackings
-  trial %@% "NumberOfFrames" <- num_frames
-  trial %@% "PercentNA" <- mistrackings / num_frames
-  trial
-}
-
-
 #' Compute the average duration of tracked gazes
 #'
 #' To quantify "flicker" in eye-tracking data, Wass, Smith and Johnson (2013)
@@ -48,10 +16,7 @@ ComputeGazeContact <- function(...) UseMethod("ComputeGazeContact")
 
 #' @export
 ComputeGazeContact.list <- function(trials) {
-  classes <- class(trials)
-  trials <- lapply(trials, ComputeGazeContact)
-  class(trials) <- classes
-  trials
+  trial_lapply(trials, ComputeGazeContact)
 }
 
 #' @export
@@ -60,6 +25,33 @@ ComputeGazeContact.Trial <- function(trial) {
   uninterrupted <- looks$lengths[!is.na(looks$values)]
   contact <- round(mean(uninterrupted) * lwl_constants$ms_per_frame)
   trial %@% "AvgGazeContact" <- contact
+  trial
+}
+
+
+
+#' Calculate the amount of missing data in a list of Trials
+#'
+#' @inheritParams ComputeGazeContact
+#' @param column A character string that names one of the columns of Gazedata in
+#'   the trial object. Default is `"GazeByImageAOI"`.
+#' @return A list of Trial objects that has been updated to include attributes
+#'   containing percentage of mistracked data.
+#' @export
+CalculateMistrackings <- function(...) UseMethod("CalculateMistrackings")
+
+#' @export
+CalculateMistrackings.list <- function(trials, column = "GazeByImageAOI") {
+  trial_lapply(trials, CalculateMistrackings, column)
+}
+
+#' @export
+CalculateMistrackings.Trial <- function(trial, column = "GazeByImageAOI") {
+  mistrackings <- sum(is.na(trial[[column]]))
+  num_frames <- length(trial[[column]])
+  trial %@% "MistrackedFrames" <- mistrackings
+  trial %@% "NumberOfFrames" <- num_frames
+  trial %@% "PercentNA" <- mistrackings / num_frames
   trial
 }
 
@@ -78,30 +70,29 @@ ComputeGazeContact.Trial <- function(trial) {
 
 
 
-## Functions to interpolate missing data in a Trial or list of Trials
 
-InterpolateMissingFrames <- function(...) {
-  UseMethod('InterpolateMissingFrames')
+
+#' Interpolate missing gaze data
+#'
+#' @inheritParams ComputeGazeContact
+#' @param window the time window (in ms.) to interpolate over
+#' @return the list of Trials with missing data interpolated
+#' @export
+InterpolateMissingFrames <- function(...) UseMethod('InterpolateMissingFrames')
+
+#' @export
+InterpolateMissingFrames.list <- function(trials, window = lwl_opts$get("interpolation_window")) {
+  trial_lapply(trials, InterpolateMissingFrames, window)
 }
 
-# Version of the function for lists of Trials
-InterpolateMissingFrames.list <- function(listOfTrials) {
-  # Preserve classes of the inputted list
-  classes <- class(listOfTrials)
-
-  # Call the Trial version of the function on each of the trials.
-  listOfTrials <- Map(InterpolateMissingFrames, listOfTrials)
-
-  # Reinstate classes on the list
-  class(listOfTrials) <- classes
-  listOfTrials
-}
-
-
-
+#' @export
 InterpolateMissingFrames.Trial <- function(trial, window = lwl_opts$get("interpolation_window")) {
-  # Convert the window duration (ms) into the number of frames in that window,
-  # rounded down.
+  message(trial %@% "TrialNo")
+  trial %@% "InterpolatedPoints" <- 0
+  trial %@% "CorrectedFrames" <- numeric(0)
+  trial %@% "CorrectedTimes" <- numeric(0)
+
+  # Convert window duration (ms) into the number of frames, rounded down.
   frames_in_window <- floor(window / lwl_constants$ms_per_frame)
 
   # Extract the gazes from the trial. Record how many missing frames there are.
@@ -111,91 +102,75 @@ InterpolateMissingFrames.Trial <- function(trial, window = lwl_opts$get("interpo
   # Grab all the non-NA gaze frames.
   tracked <- which(!is.na(gazes))
 
-  # For each non-NA gaze frame, subtract the index of the previous frame from it.
-  # The difference allows us to find out how many NA frames were skipped when we
-  # extracted all the non-NA frames.
-  differences <- tracked
-  for (frame in seq(from = length(differences), to = 1)){
-    if (1 < frame) {
-      differences[frame] <- (differences[frame] - differences[frame - 1])
-    }
-  }
+  # The lag in frame numbers of non-NA gazes tells us how many NA frames were
+  # skipped when we extracted all the non-NA gazes. Include the 0 at front
+  # because diff(1:n) returns n-1 values
+  differences <- diff(c(0, tracked))
 
-  ## Find the precise starts and ends of each gap
+  ## Find starts and ends of each NA gap
 
-  # Starts are not accurate because they don't take into account other missing
-  # frames. Use the cumulative sum of missing frames to correct the start
-  # locations.
+  # Locations from `which` are not accurate because they don't take into account
+  # earlier missing frames. Use the cumulative sum of missing frames to correct
+  # these start locations.
+
   gap_start <- which(1 < differences)
   gap_size <- differences[gap_start] - 1
-  gap_sizes <- cumsum(gap_size)
-
-  # Correction of gap locations
-  for (gap in seq_along(gap_start)){
-    if (gap == 1){
-      next
-    }
-    gap_start[gap] <- gap_start[gap] + gap_sizes[gap - 1]
-  }
-
-  # Final adjustments so gap_start and gap_end are indices are non-NA values.
-  gap_start <- gap_start - 1
+  total_gap_sizes <- cumsum(gap_size)
+  # First gap doesn't need to be offset
+  start_offsets <- c(0, total_gap_sizes[-length(total_gap_sizes)])
+  gap_start <- gap_start + start_offsets - 1
   gap_end <- gap_start + gap_size + 1
 
-  # Exclude gaps larger than frames_in_window frames from consideration
-  correctable <- which(gap_size <= frames_in_window)
+  # Enforce valid windows! Margins need to be non-NA and next to an NA value
+  stopifnot(is.na(gazes[c(gap_start + 1, gap_end - 1)]),
+            !is.na(gazes[c(gap_start, gap_end)]))
 
-  if (length(correctable) == 0) {
-    trial %@% "InterpolatedPoints" <- 0
-    trial %@% "CorrectedFrames" <- NA
-    return(trial)
+  # Make a set of Gap objects from these start/end/size descriptions
+  gaps <- Map(Gap, gap_start, gap_end, gap_size)
+
+  # Only fill gaps no bigger than the interpolation window, gaps that don't
+  # involve first frame and gaps with the gaze location on both sides of window
+  has_legal_length <- function(gap) gap$na_size <= frames_in_window
+  is_not_first_frame <- function(gap) gap$start != 0
+  is_fillable <- function(gap) gazes[gap$start] == gazes[gap$end]
+
+  gaps <- Filter(has_legal_length, gaps)
+  gaps <- Filter(is_not_first_frame, gaps)
+  gaps <- Filter(is_fillable, gaps)
+
+  # Fill each gap
+  for (gap in gaps) {
+    trial <- fill_gap(trial, gap)
   }
 
-  gap_start <- gap_start[correctable]
-  gap_end <- gap_end[correctable]
-  gap_size <- gap_size[correctable]
-
-  # Exclude the very first gap if it includes the very first frame
-  if (gap_start[1] == 0){
-    gap_start <- gap_start[-1]
-    gap_end <- gap_end[-1]
-    gap_size <- gap_size[-1]
-  }
-
-  # Get the values before and after each NA gap
-  start_values <- gazes[gap_start]
-  end_values <- gazes[gap_end]
-
-  corrected_frames <- NULL
-
-  # If the values before and after the gaps match, interpolate
-  for (point in seq_along(gap_start)){
-    if (start_values[point] == end_values[point]){
-      value <- start_values[point]
-      gazes[gap_start[point]:gap_end[point]] <- value
-
-      columns <- c("XLeft", "XRight", "XMean", "YLeft", "YRight", "YMean",
-                   "XLeftToTarget", "XRightToTarget", "XMeanToTarget",
-                   "YLeftToTarget", "YRightToTarget", "YMeanToTarget")
-
-      means <- sapply(trial[gap_start[point]:gap_end[point], columns],
-                      mean, na.rm = TRUE)
-
-      na_start <- gap_start[point] + 1
-      na_end <- gap_end[point] - 1
-
-      corrected_frames <- c(corrected_frames, na_start:na_end)
-
-      for (col in columns) {
-        trial[na_start:na_end, col] <- means[col]
-      }
-    }
-  }
-
-  # Update the trial
-  trial %@% "CorrectedFrames" <- corrected_frames
-  trial %@% "InterpolatedPoints" <- missing - sum(is.na(gazes))
-
-  # Update the AOI Data
+  # Update the AOI Data using the interpolated values
   AddAOIData(trial)
 }
+
+# Interpolate a single gap
+fill_gap <- function(trial, gap) {
+  # Compute interpolation values
+  columns <- c("XLeft", "XRight", "XMean", "YLeft", "YRight", "YMean",
+               "XLeftToTarget", "XRightToTarget", "XMeanToTarget",
+               "YLeftToTarget", "YRightToTarget", "YMeanToTarget")
+  means <- sapply(trial[gap$seq, columns], mean, na.rm = TRUE)
+
+  # Interpolate each column
+  for (col in columns) {
+    trial[gap$na_seq, col] <- means[col]
+  }
+
+  trial %@% "CorrectedFrames" <- c(trial %@% "CorrectedFrames", gap$na_seq)
+  trial %@% "CorrectedTimes" <- trial$Time[trial %@% "CorrectedFrames"]
+  trial %@% "InterpolatedPoints" <- trial %@% "InterpolatedPoints" + gap$na_size
+  trial
+}
+
+# Simple container for the information we care about when interpolating a gap
+Gap <- function(start, end, na_size) {
+  structure(list(
+    start = start, end = end, na_size = na_size,
+    seq = seq(start, end), na_seq = seq(start + 1, end - 1)),
+    class = c("Gap", "list"))
+}
+
