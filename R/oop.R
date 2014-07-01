21256239d8a803a65a13eaa5608d8bdd88b5a010
TJ Mahr <mahr@wisc.edu>
2014-07-01 14:46:39 -0500

    update object-oriented programming functions

--- R/oop.R
+++ R/oop.R
@@ -1,29 +1,51 @@
-# This is where we generic methods for LWL objects.
+## S3 methods and other functions that care about classes of objects
 
 
-## Combine lists of Trials.
+
+
+#' Apply a function to a list of trials, preserving classes of the list
+#'
+#' This function is just a wrapper for \code{lapply} that makes sure that the
+#' classes of the inputted list are preserved in the outputted list.
+#'
+#' @param trials a list of Trial objects
+#' @param trial_func a function to apply on each Trial object
+#' @param ... additional arguments passed to the function
+#' @export
+trial_lapply <- function(trials, trial_func, ...) {
+  classes <- class(trials)
+  trials <- lapply(trials, trial_func, ...)
+  class(trials) <- classes
+  trials
+}
+
+
+# Combine lists of Trials.
+#' @export
 c.Session <- function(session, ...) {
   # Coerce session to a list and then call the c.list method on session and ...
   class(session) <- 'list'
   trials <- c(session, ...)
-  
+
   # If there are multiple subjects, return a "Task".
-  if (UniqueTask(trials)) {
-    if (UniqueSubject(trials)) as.Session(trials) else as.Task(trials)
-  } 
+  if (unique_task(trials)) {
+    if (unique_subject(trials)) as.Session(trials) else as.Task(trials)
+  }
   # WE may need "else trials" if we ever want to combine trials with different
   # tasks.
 }
 
 # The checks inside c.Session should work for combining Task objects.
+#' @export
 c.Task <- function(task, ...) c.Session(task, ...)
 
-## Coerce a list of Trials into a Session or Task
+#' @export
 as.Session <- function(x) {
   if (!is.Session(x)) class(x) <- c("Session", "list")
   x
 }
 
+#' @export
 as.Task <- function(x) {
   if (!is.Task(x)) class(x) <- c("Task", "list")
   x
@@ -46,10 +68,10 @@ print.Stimdata <- function(...) str(...)
 
 
 # Check whether trials should be a Task or Session object.
-UniqueAttribute <- function(attr_name) { 
+unique_attr_checker <- function(attr_name) {
   function(trials) n_distinct(trials %@% attr_name) == 1
-} 
-UniqueTask <- UniqueAttribute("Task")
-UniqueSubject <- UniqueAttribute("Subject")
+}
+unique_task <- unique_attr_checker("Task")
+unique_subject <- unique_attr_checker("Subject")
 
 n_distinct <- function(xs) length(unique(xs))
